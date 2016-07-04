99799098efce2c217d1161f30e685ff14babe33e
Harshil Agrawal <ghagrawal17@gmail.com>
2016-07-05 00:18:24 +0530

    Create Factorialize A Number.js
    Return the factorial of the provided integer.
    
    If the integer is represented with the letter n, a factorial is the product of all positive integers less than or equal to n.
    
    Factorials are often represented with the shorthand notation n!
    
    For example: 5! = 1 * 2 * 3 * 4 * 5 = 120

---
+++ Factorialize A Number.js
@@ -0,0 +1,18 @@
+
+function factorialize(num) {
+   var myArray = [];
+
+  var multiplyMe = 1;
+
+  for(var i = 1; i <= num; i++){
+   multiplyMe *= myArray.push(i);
+  }
+
+ 
+
+  return multiplyMe;   
+
+
+}
+
+factorialize(5);
